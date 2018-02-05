aae6659573565b081cffcb17d929c096034bcfa8
Vezmo <olivier.vezina@hotmail.com>
2018-02-04 21:02:01 -0500

    MessingAround
    MessingAround

---
+++ SWW Redux/Assets/Scripts/rotatetest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatetest : MonoBehaviour {
+
+  public float Speed;
+  RectTransform transform;
+
+	// Use this for initialization
+	void Start ()
+  {
+    transform = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    transform.Rotate(new Vector3(0, 0, -Speed * Time.deltaTime));
+
+
+  }
+}
